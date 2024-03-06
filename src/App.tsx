06da1b6e26a85fbd06c7ea9749249c71d7e7eae4
Rafael <contato.rafaelgomes@outlook.com>
2024-03-06 11:45:56 -0300

    initial config done!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from './App.module.css';
+function App() {
+
+  return (
+    <React.Fragment>
+      <h1>Hello Wolrd!</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
