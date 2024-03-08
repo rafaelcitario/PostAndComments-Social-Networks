2f83143e11cc855e8fa2b00b02938e875942d1f3
Rafael <contato.rafaelgomes@outlook.com>
2024-03-08 15:30:18 -0300

    interations whit comment is done!

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,8 +1,9 @@
 import styles from './Avatar.module.css';
 interface AvatarProps {
   src: string,
+  hasBorder?: boolean
 }
-export function Avatar({ src }: AvatarProps) {
+export function Avatar({ src, ...props }: AvatarProps) {
 
   return (
     <img className={styles.avatar} src={src} alt="" />
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,7 @@
-import { Trash } from '@phosphor-icons/react';
+import { HandsClapping, Trash } from '@phosphor-icons/react';
 import { Avatar } from './Avatar';
 import styles from './Comments.module.css';
+import { MouseEvent, useState } from 'react';
 
 const avatar = "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=30&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
@@ -10,25 +11,48 @@ interface CommentsProps {
 }
 
 export function Comments({ ...props }: CommentsProps) {
-
+  const [clapsNumber, setClapsNumber] = useState(0);
+  const [addClassToClapButton, setAddClassToClapButton] = useState('');
   function handleDeleteCommentary() {
     props.deleteComment(props.content);
   }
 
+  function handleCountClapsComment(): void {
+    setClapsNumber(clapsNumber + 1);
+    setAddClassToClapButton(`${styles.clapped}`)
+  }
+
+  let isNotClaps: number | string = clapsNumber !== 0 ? clapsNumber : "";
+
   return (
     <div className={styles.commentBox}>
       <Avatar src={avatar} />
-
-      <div className={styles.commentContent}>
-        <header>
-          <div className={styles.authorAndTime}>
-            <strong>Visitante</strong>
-            <time>há 2 min</time>
-          </div>
-          <button onClick={handleDeleteCommentary} ><Trash size={24} weight='bold' /></button>
-        </header>
-        <p>{props.content}</p>
+      <div>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Visitante</strong>
+              <time>há 2 min</time>
+            </div>
+            <button onClick={handleDeleteCommentary} ><Trash size={24} weight='bold' /></button>
+          </header>
+          <p>{props.content}</p>
+        </div>
+        <div className={styles.clapsAndNumberClaps}>
+          <button onClick={handleCountClapsComment}>
+            <HandsClapping
+              className={addClassToClapButton}
+              size={20}
+              weight='bold'
+            />
+          </button>
+          <span>
+            {isNotClaps}
+          </span>
+        </div>
       </div>
+
+
     </div>
   );
 }
