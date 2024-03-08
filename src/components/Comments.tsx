4776a050a1a0a4c8be92d67d12e56575f0971f08
Rafael <contato.rafaelgomes@outlook.com>
2024-03-08 02:45:36 -0300

    delete coment done

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,44 +1,34 @@
-import { Trash, TrashSimple } from '@phosphor-icons/react';
+import { Trash } from '@phosphor-icons/react';
 import { Avatar } from './Avatar';
 import styles from './Comments.module.css';
 
 const avatar = "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=30&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-
-
-// Button delete commentaries
-// function handleDeleteCommentary(event: <HTMLButtonElement>): void {
-//   console.log(event);
-
-// }
-
-
-
 interface CommentsProps {
   content: string,
+  deleteComment: (content: string) => void
 }
 
+export function Comments({ ...props }: CommentsProps) {
 
+  function handleDeleteCommentary() {
+    props.deleteComment(props.content);
+  }
 
-export function Comments({ content }: CommentsProps) {
   return (
     <div className={styles.commentBox}>
       <Avatar src={avatar} />
+
       <div className={styles.commentContent}>
         <header>
           <div className={styles.authorAndTime}>
-            <strong>Rafael Citario</strong>
+            <strong>Visitante</strong>
             <time>há 2 min</time>
           </div>
-          <button ><Trash size={24} weight='bold' /></button>
+          <button onClick={handleDeleteCommentary} ><Trash size={24} weight='bold' /></button>
         </header>
-        <p>{content}</p>
+        <p>{props.content}</p>
       </div>
-
-
-
-
-
     </div>
   );
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,45 +1,67 @@
-import { Avatar } from './Avatar';
-import { format, formatDistanceToNow } from 'date-fns'
-import styles from './Post.module.css';
-import { ptBR } from 'date-fns/locale/pt-BR';
-import { Comments } from './Comments';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { Avatar } from "./Avatar";
+import { format, formatDistanceToNow } from "date-fns";
+import styles from "./Post.module.css";
+import { ptBR } from "date-fns/locale/pt-BR";
+import { Comments } from "./Comments";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
 interface Author {
-  name: string,
-  role: string,
-  avatarUrl: string,
+  name: string;
+  role: string;
+  avatarUrl: string;
 }
 
 interface Content {
-  type: string,
-  content: string
+  type: string;
+  content: string;
 }
 
 interface PostProps {
-  author: Author,
-  content: Content[]
-  publishedAt: Date,
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
 }
 
 export function Post({ author, content, publishedAt }: PostProps) {
-  const [commentsList, setCommentsList] = useState<string[]>(['SALVE RAPAZIADA']);
-  const [currentComment, setCurrentComment] = useState<string>('');
+  const [commentsList, setCommentsList] = useState<string[]>([]);
+  const [currentComment, setCurrentComment] = useState<string>("");
 
+  const postAtDate = format(new Date(publishedAt), "MM/dd/yyyy kk:mm:ss");
+  const postFewDatesAgo = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    includeSeconds: true,
+    addSuffix: true,
+  });
 
-  const postAtDate = format(new Date(publishedAt), 'MM/dd/yyyy kk:mm:ss');
-  const postFewDatesAgo = formatDistanceToNow(publishedAt, { locale: ptBR, includeSeconds: true, addSuffix: true })
-
-
-
-  function handleSubmitComment(event: FormEvent) {
+  function handleSubmitComment(event: FormEvent): void {
     event.preventDefault();
     setCommentsList([...commentsList, currentComment]);
-    console.log(currentComment);
+    setCurrentComment("");
   }
 
-  function handleCommentPassToState(event: ChangeEvent<HTMLTextAreaElement>) {
+  function handleCommentPassToState(
+    event: ChangeEvent<HTMLTextAreaElement>
+  ): void {
     setCurrentComment(event.target.value);
+    event.target.setCustomValidity("");
+  }
+
+  function handleIsInvalidComment(
+    event: InvalidEvent<HTMLTextAreaElement>
+  ): void {
+    const validityMessage =
+      "Como é amigo? 🤨 Deixa isso aqui vazio não, por favor!";
+    event.target.setCustomValidity(validityMessage);
+  }
+
+
+
+  function handleDeletComment(content: string) {
+    const commentListWithoutDeleteOne = commentsList.filter(contentToDelete => {
+      return content !== contentToDelete;
+    })
+
+    setCommentsList(commentListWithoutDeleteOne);
   }
 
 
@@ -54,44 +76,54 @@ export function Post({ author, content, publishedAt }: PostProps) {
             <span>{author.role}</span>
           </div>
           <time title={postAtDate} dateTime={postAtDate}>
-            {postFewDatesAgo.concat(' atrás.')}
+            {postFewDatesAgo.concat(" atrás.")}
           </time>
         </div>
       </header>
+
       <div className={styles.content}>
-        {content.map(content => {
-          if (content.type == 'paragraph') {
-            return <p key={content.content}>{content.content}</p>
+        {content.map((content) => {
+          if (content.type == "paragraph") {
+            return <p key={content.content}>{content.content}</p>;
           } else {
-            return <p key={content.content}><a href="">{content.content}</a></p>;
+            return (
+              <p key={content.content}>
+                <a href="">{content.content}</a>
+              </p>
+            );
           }
         })}
       </div>
 
       <footer>
-        <form
-          onSubmit={handleSubmitComment}
-          className={styles.formFooter}>
+        <form onSubmit={handleSubmitComment} className={styles.formFooter}>
           <strong>Deixe seu comentário</strong>
+
           <textarea
+            required
+            title="compartilhe suas idéias"
+            value={currentComment}
+            onInvalid={handleIsInvalidComment}
             onChange={handleCommentPassToState}
-            name='entryCommentField'
-            placeholder='o que você pensa sobre isso?'></textarea>
-          <button type='submit'>Publicar</button>
+            placeholder="o que você pensa sobre isso?"
+          ></textarea>
+
+          <div className={styles.publishButtonContainer}>
+            <button disabled={currentComment === "" ? true : false} type="submit">
+              Publicar
+            </button>
+          </div>
+
         </form>
       </footer>
 
       {commentsList.map((comment) => {
-        return (
-          <Comments
-            key={comment}
-            content={comment}
-          />
-        )
+        return <Comments
+          key={comment}
+          content={comment}
+          deleteComment={handleDeletComment}
+        />;
       })}
     </div>
-
-
-  )
-
+  );
 }
