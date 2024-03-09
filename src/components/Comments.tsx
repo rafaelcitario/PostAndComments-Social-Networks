c52d802dd122caca5e5753eb7aa15c1b27aab5cf
Rafael <contato.rafaelgomes@outlook.com>
2024-03-08 22:46:22 -0300

    -a

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,7 +1,7 @@
 import { HandsClapping, Trash } from '@phosphor-icons/react';
 import { Avatar } from './Avatar';
 import styles from './Comments.module.css';
-import { MouseEvent, useState } from 'react';
+import { useState } from 'react';
 
 const avatar = "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=30&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
