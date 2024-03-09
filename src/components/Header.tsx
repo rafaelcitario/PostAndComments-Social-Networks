e3c4a1bd8923b620c1e6a8ee7118133c260abcf2
Rafael <contato.rafaelgomes@outlook.com>
2024-03-08 23:23:41 -0300

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,6 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={note} alt="logo icone bloco de notas auto colante" />
-      <nav className={styles.navbar}>
-        <ul >
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
-      </nav>
     </header>
   )
 }
